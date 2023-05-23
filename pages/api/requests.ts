1001dccc291dabe01aa689e44176bfbf40baed0b
ramy <ramy_3@walla.com>
2023-05-23 13:39:17 +0300

    fix axios

--- pages/api/requests.ts
+++ pages/api/requests.ts
@@ -19,7 +19,7 @@ const read = async (req: NextApiRequest, res: NextApiResponse) => {
 
     const cityID = resp.data.data.SETTLEMENT[0].ObjectID;
 
-    resp = await axios.get(
+    resp = await axios.post(
       `https://www.police.gov.il/MapSkifoutService?city=${cityID}&street=&house=&subjects=1,2,10,11,9,12,13,19,16,28,34,31,22,24,23,37,39,38,14,20,17,29,35,32,15,21,18,30,36,33&quarter=1,2,3,4&year=${req.body.year}`
     );
 
